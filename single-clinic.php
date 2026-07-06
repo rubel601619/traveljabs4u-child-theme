8c828b7005c71d79a615b165389495181cb620f3
Rubel Mahmud <rubel601619@gmail.com>
2026-07-06 18:43:38 +0600

    feat: update single clinic page layout with improved address and location display

--- single-clinic.php
+++ single-clinic.php
@@ -8,6 +8,12 @@
 	ul{
 		text-align: left !important;
 	}
+	.clinic-link{
+		color: #444 !important;
+		:hover{
+			color: #7ecb2a !important;
+		}
+	}
 </style>
 
 <section class="pageBanner vert-mid" style="background-image: url('<?= get_the_post_thumbnail_url(); ?>')">
@@ -21,61 +27,79 @@
 	</div>
 </section>
 
-<section class="single-post-section single-branch-content pb-5 mb-5 pt-5 mt-5">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8 col-md-12 branchPage">
-				<?php the_content(); ?>
-				<hr>
-				<div class="row">
-					<div class="col-lg-6">
-						<h3><i class="fa-sharp fa-solid fa-location-dot"></i> Address</h3>
-						<p><?php the_field( 'clinic_address' ); ?></p>
-						<?php if ( get_field( 'clinic_postcode' ) ) : ?>
-							<p><?php the_field( 'clinic_postcode' ); ?></p>
-						<?php endif; ?>
-				</div>
-					<div class="col-lg-6">
-						<?php
-						$lat = get_field( 'clinic_latitude' );
-						$lng = get_field( 'clinic_longitude' );
-						if ( $lat && $lng ) :
-						?>
-						<h3><i class="fa-solid fa-map"></i> Location</h3>
+<section class="clearfix py-5" style="background-color: #F8FAFC;">
+	<div class="container py-lg-4">
+		<div class="row g-lg-5">
+			<div class="col-lg-8">
+				<?php 
+				if ( ! empty( get_the_content() ) ) : ?>
+					<div class="mb-4">
+						<?php the_content(); ?>
+					</div>
+				<?php endif; ?>
+				<?php
+					$lat = get_field( 'clinic_latitude' );
+					$lng = get_field( 'clinic_longitude' );
+				?>
+				<?php if ( $lat && $lng ) : ?>
+					<div class="bg-white p-3">
+						<div class="fs-5 mb-3 fw-bold"><i class="fa-solid fa-map"></i> Location</div>
 						<iframe
 							width="100%"
-							height="300"
+							height="400"
 							style="border:0;border-radius:8px;"
 							loading="lazy"
 							referrerpolicy="no-referrer-when-downgrade"
 							src="https://maps.google.com/maps?q=<?php echo esc_attr( $lat ); ?>,<?php echo esc_attr( $lng ); ?>&z=15&output=embed">
 						</iframe>
-						<?php endif; ?>
+					</div>
+				<?php endif; ?>
+			</div>
+			<div class="col-lg-4">
+				<div class="bg-white p-3 border">
+					<div class="fs-5 mb-3"><i class="fa-sharp fa-solid fa-location-dot"></i> Address</div>
+					<p><?php the_field( 'clinic_address' ); ?></p>
+					<?php if ( get_field( 'clinic_postcode' ) ) : ?>
+						<p class="mb-0"><strong>Post Code :</strong> <?php the_field( 'clinic_postcode' ); ?></p>
+					<?php endif; ?>
+
+				</div>
+				<div class="bg-white p-3 border mt-4">
+					<div class="fs-5 mb-3"><i class="fa-sharp fa-solid fa-location-dot"></i> Others Clinics</div>
+					<ul class="ps-4 m-0" style="max-height: 400px; overflow-y: auto;">
+						<?php
+						$the_query = new WP_Query( [
+							'post_type'      => 'clinic',
+							'posts_per_page' => -1,
+							'post__not_in'   => [ get_the_ID() ],
+							'orderby'        => 'title',
+							'order'          => 'ASC',
+						] );
+
+						if ( $the_query->have_posts() ) {
+							while ( $the_query->have_posts() ) {
+								$the_query->the_post();
+								?>
+								<li><a href="<?php the_permalink(); ?>" class="text-decoration-none text-link clinic-link"><?php the_title(); ?></a></li>
+								<?php
+							}
+						}
+						wp_reset_postdata();
+						?>
+					</ul>
 				</div>
 			</div>
-			<div class="col-lg-4 col-md-12 sidebar">
-				<h3>Other Clinics</h3>
-				<ul class="vaccinationList">
-					<?php
-					$the_query = new WP_Query( [
-						'post_type'      => 'clinic',
-						'posts_per_page' => -1,
-						'post__not_in'   => [ get_the_ID() ],
-						'orderby'        => 'title',
-						'order'          => 'ASC',
-					] );
-
-					if ( $the_query->have_posts() ) {
-						while ( $the_query->have_posts() ) {
-							$the_query->the_post();
-							?>
-							<li><a href="<?php the_permalink(); ?>"><?php the_title( '<h4>', '</h4>' ); ?></a></li>
-							<?php
-						}
-					}
-					wp_reset_postdata();
-					?>
-				</ul>
+		</div>
+		
+		<div class="row">
+			<div class="col-lg-8">
+				
+				<hr>
+				<div class="row">
+					
+					<div class="col-lg-6">
+						
+				</div>
 			</div>
 		</div>
 	</div>
